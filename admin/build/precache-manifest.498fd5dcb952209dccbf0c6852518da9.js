4f027bff7c335b9866f913fd4296a43be3219681
Iuliu Visovan <iuliuvisovan@usource.ro>
2020-09-17 00:48:09 +0300

    build

---
+++ admin/build/precache-manifest.498fd5dcb952209dccbf0c6852518da9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4726455fa32a0c3cdd5f",
+    "url": "/static/css/main.d2b0e4c0.chunk.css"
+  },
+  {
+    "revision": "4726455fa32a0c3cdd5f",
+    "url": "/static/js/main.4726455f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2e573bb0e57b4c480d2f",
+    "url": "/static/js/2.2e573bb0.chunk.js"
+  },
+  {
+    "revision": "a549a725e51c6cb89c829394419cdeaf",
+    "url": "/index.html"
+  }
+];
