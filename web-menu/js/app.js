75f20d4d9190db91061111c9510cf638b0f4fa3d
Iuliu Visovan <iuliuvisovan@usource.ro>
2020-09-28 22:25:14 +0300

    without polyfill

--- web-menu/js/app.js
+++ web-menu/js/app.js
@@ -1,3 +1,5 @@
+// polyfill();
+
 document.querySelectorAll('.category-button').forEach((x) => {
   x.addEventListener('click', function () {
     if (this.classList.contains('active')) {
@@ -5,5 +7,11 @@ document.querySelectorAll('.category-button').forEach((x) => {
     } else {
       this.classList.add('active');
     }
+  });
+});
+
+document.querySelectorAll('input[type=text]').forEach((x) => {
+  x.addEventListener('focus', function () {
+    window.scrollTo({ top: 198, behavior: 'smooth' });
   });
 });
