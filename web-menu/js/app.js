4a6dbe95d3a550fdc5d8fb631a80eb4dacb9fd6e
Iuliu Visovan <iuliuvisovan@usource.ro>
2020-10-05 22:10:47 +0300

    add covid questionnaire UI

--- web-menu/js/app.js
+++ web-menu/js/app.js
@@ -17,3 +17,44 @@ document.querySelectorAll('input[type=text]').forEach((x) => {
     });
   }, 300);
 });
+
+const dateInput = document.querySelector('#dateTime');
+const date = new Date();
+dateInput.value = `${(date.getDate() + '').padStart(2, '0')}.${(date.getMonth() + 1 + '').padStart(2, '0')} @ ${(date.getHours() + '').padStart(2, '0')}:${(date.getMinutes() + '').padStart(2, '0')}`;
+
+document.querySelectorAll('.field').forEach((x) => {
+  x.addEventListener('keyup', (e) => {
+    checkFormValidity();
+  });
+});
+
+const submitButton = document.querySelector('#seeMenuButton');
+submitButton.addEventListener('click', () => {
+  document.querySelector('.pre-menu').remove();
+  window.scrollTo(0, 0);
+});
+
+function checkFormValidity() {
+  const suppliedName = document.querySelector('#fullName').value;
+  const suppliedPhoneNumber = document.querySelector('#phoneNumber').value;
+
+  if (suppliedName.length < 2) {
+    submitButton.disabled = true;
+
+    return;
+  }
+
+  if (suppliedPhoneNumber.length < 8) {
+    submitButton.disabled = true;
+
+    return;
+  }
+
+  if (!/^[0-9\s\+]+$/.test(suppliedPhoneNumber)) {
+    submitButton.disabled = true;
+
+    return;
+  }
+
+  submitButton.disabled = false;
+}
